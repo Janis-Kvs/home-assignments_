28b766abb31ec8ffdcf8da73b3b0c0de9986d92d
Janis Kazjonovs <janis.kazjonovs@gmail.com>
2021-09-20 11:43:30 +0300

    Commit

--- assignments/Quotatis/assets/js/global.js
+++
@@ -1,8 +0,0 @@
-// Global
-import "./modules/bootstrap";
-import "./modules/theme";
-import "./modules/dragula";
-import "./modules/feather";
-import "./modules/moment";
-import "./modules/sidebar";
-import "./modules/notyf";
